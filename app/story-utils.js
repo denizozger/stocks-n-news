64f9d8840ce6c0ba4815c55057e6c845914c3eb7
Deniz Ozger <denizozger@gmail.com>
2016-08-09 22:40:18 +0100

    Unit tests for story sentiments

--- app/story-utils.js
+++ app/story-utils.js
@@ -1,5 +1,29 @@
 'use strict';
 
-exports.foo =  () => {
-	return 'bar';
+const
+	positiveWords = ['positive', 'success', 'grow', 'gains', 'happy', 'healthy'],
+	negativeWords = ['disappointing', 'concerns', 'decline', 'drag', 'slump', 'feared'];
+
+exports.getSentimentOfStory =  story => {
+	let wordsInStory = removePunctiationAndWhitespace(story.body).split(' ');
+	return getPositiveWordCount(wordsInStory) - getNegativeWordCount(wordsInStory);
+}
+
+function getPositiveWordCount(words) {
+	return getNumberOfCommonElementsOfArrays(words, positiveWords);
+}
+
+function getNegativeWordCount(words) {
+	return getNumberOfCommonElementsOfArrays(words, negativeWords);
+}
+
+function getNumberOfCommonElementsOfArrays(array1, array2) {
+	return array1
+		.filter(el => array2.indexOf(el) !== -1)
+		.length;
+}
+
+function removePunctiationAndWhitespace(s) {
+	return s.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
+		.replace(/\s{2,}/g," ");
 }
--- test/test-unit.js
+++ test/test-unit.js
@@ -7,7 +7,14 @@ const
 describe('Unit Tests', function() {
 
   it('should analyse story sentiment', function() {
-    assert.equal(storyUtils.foo(), 'bar');
+		const positiveStory = 'grow gains happy healthy ʘ‿ʘ'
+  	assert.equal(storyUtils.getSentimentOfStory({body: positiveStory}), 4);
+
+  	const negativeStory = 'concerns decline disappointing (╯°□°）╯︵ ┻━┻'
+    assert.equal(storyUtils.getSentimentOfStory({body: negativeStory}), -3);
+
+  	const neutralStory = 'positive but disappointing, gains but decline ಠ_ಠ'
+    assert.equal(storyUtils.getSentimentOfStory({body: neutralStory}), 0);
   });
   
 });
