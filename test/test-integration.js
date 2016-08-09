8c162558e8699533e5670237609f309f4836c860
Deniz Ozger <denizozger@gmail.com>
2016-08-09 20:38:51 +0100

    Empty unit and integration tests

---
+++ test/test-integration.js
@@ -0,0 +1,24 @@
+const 
+	assert = require('assert');
+
+describe('Integration Tests', function() {
+
+  describe('Database', function() {
+    it('should fetch ticket codes', function() {
+      assert(true);
+    });
+  });
+
+  describe('Stock API', function() {
+    it('should fetch stock prices', function() {
+      assert(true);
+    });
+  });
+
+  describe('Story API', function() {
+    it('should fetch news stories', function() {
+      assert(true);
+    });
+  });
+
+});
---
+++ test/test-unit.js
@@ -0,0 +1,10 @@
+const 
+	assert = require('assert');
+
+describe('Unit Tests', function() {
+
+  it('should analyse story sentiment', function() {
+    assert(true);
+  });
+  
+});
